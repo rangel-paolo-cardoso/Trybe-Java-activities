bb2ee188f1dab69d5c8b2d59ae2e70604def2a50
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-18 00:10:12 -0300

    Added Javadoc

--- dates/dates-with-times/income-simulator/src/main/java/com/rangel/IncomeSimulator.java
+++ dates/dates-with-times/income-simulator/src/main/java/com/rangel/IncomeSimulator.java
@@ -4,11 +4,18 @@ import java.time.LocalDateTime;
 
 public class IncomeSimulator {
 
-    private static final double INCOME_RATE = 10 / 100.0D;
-    
-    public static int simulateIncomeInYears(double initialAmount, double targetAmount) {
+    private final double INCOME_RATE = 10 / 100.0D;
+
+    /**
+     * Simulates an income in years.
+     * @param initialAmount type double, the initial income application.
+     * @param targetAmount type double, the desired amount.
+     * @return
+     */
+    public int simulateIncomeInYears(double initialAmount, double targetAmount) {
         LocalDateTime time = LocalDateTime.now();
-        int numberOfYears = (int) (Math.log(targetAmount / initialAmount) / Math.log(1 + INCOME_RATE));
+        int numberOfYears = (int) Math.ceil(
+                Math.log(targetAmount / initialAmount) / Math.log(1 + INCOME_RATE));
 
         LocalDateTime futureTime = time.plusYears(numberOfYears);
         return futureTime.getYear();
