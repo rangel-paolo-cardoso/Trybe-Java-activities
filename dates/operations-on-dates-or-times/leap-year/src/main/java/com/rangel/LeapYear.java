523dc87ac677e324c385ef58a9a6a6e808c2cd86
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-24 23:27:02 -0300

    Leap year system implemented

---
+++ dates/operations-on-dates-or-times/leap-year/src/main/java/com/rangel/LeapYear.java
@@ -0,0 +1,31 @@
+package com.rangel;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LeapYear {
+    
+    /**
+     * Identifies if a given year is a leap year.
+     * @param year the parameter is of type int.
+     * @return a boolean os returned.
+     */
+    public boolean identifyLeapYear(int year) {
+        return (year % 100 == 0) || (year % 4 == 0 || year % 400 == 0);
+    }
+
+    /**
+     * Identifies what is the next leap year, giving a String representing a date.
+     * @param brasilianFormattedDate the parameter is a date in Brazilian format, as a String.
+     * @return the next leap year as an int number.
+     */
+    public int identifyNextLeapYear(String brasilianFormattedDate) {
+        DateTimeFormatter brazilianFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateFormatted = LocalDate.parse(brasilianFormattedDate, brazilianFormat).plusYears(1);
+        while (!dateFormatted.isLeapYear()) {
+            dateFormatted = dateFormatted.plusYears(1);
+        }
+
+        return dateFormatted.getYear();
+    }
+}
