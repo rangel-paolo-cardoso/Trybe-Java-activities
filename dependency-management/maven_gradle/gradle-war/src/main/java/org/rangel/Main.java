0b95ad3d19430ff55e84734c43f8a5d78e3b5621
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-07-04 22:22:30 -0300

    Using Gradle successfully

---
+++ dependency-management/maven_gradle/gradle-war/src/main/java/org/rangel/Main.java
@@ -0,0 +1,11 @@
+package org.rangel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf(greeting());
+    }
+
+    public static String greeting() {
+        return "Hello Gradle!";
+    }
+}
