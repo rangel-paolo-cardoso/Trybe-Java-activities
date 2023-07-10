401fee658783158a881da7872ddb5a7de97a8677
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-07-09 22:06:37 -0300

    Removed unused import.

--- tests_with_junit5/unit_tests_with_junit5/hr-system/src/test/java/com/rangel/CollaboratingPersonTest.java
+++ tests_with_junit5/unit_tests_with_junit5/hr-system/src/test/java/com/rangel/CollaboratingPersonTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.rangel.CollaboratingPerson;
-
 public class CollaboratingPersonTest {
 
     @Test
