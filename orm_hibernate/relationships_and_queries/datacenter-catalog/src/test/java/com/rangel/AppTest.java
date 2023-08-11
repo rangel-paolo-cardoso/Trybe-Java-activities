103551efa465e62b81e85ef21768c444718920c2
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-10 23:22:36 -0300

    Created the Application class

--- orm_hibernate/relationships_and_queries/datacenter-catalog/src/test/java/com/rangel/AppTest.java
+++ orm_hibernate/relationships_and_queries/datacenter-catalog/src/test/java/com/rangel/AppTest.java
@@ -1,20 +1,23 @@
 package com.rangel;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
 
-import org.junit.Test;
+import com.rangel.entities.Application;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    @BeforeAll
+    public static void populateDB() {
+        Application app1 = new Application();
+        app1.setName("FutuereH");
+        app1.setDescription("Java");
+
+        Application app2 = new Application();
+        app2.setName("Agro Techfields");
+        app2.setDescription("Python");
+
+        Application app3 = new Application();
+        app3.setName("Trybe Course");
+        app3.setDescription("Javascript");
     }
 }
