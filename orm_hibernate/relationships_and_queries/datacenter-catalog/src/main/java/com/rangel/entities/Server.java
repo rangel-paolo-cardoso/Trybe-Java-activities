a0d04e4e9b512f770ae48d9cd5d66c5a62b29ea4
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-12 23:14:53 -0300

    SErver class implemented

--- orm_hibernate/relationships_and_queries/datacenter-catalog/src/main/java/com/rangel/entities/Server.java
+++ orm_hibernate/relationships_and_queries/datacenter-catalog/src/main/java/com/rangel/entities/Server.java
@@ -1,5 +1,76 @@
 package com.rangel.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Server {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "datacenter_id")
+    private Datacenter datacenter;
+
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "relationship_application_server",
+            joinColumns = { @JoinColumn(name = "server_id") },
+            inverseJoinColumns = { @JoinColumn(name = "application_id") })
+    private Set<Application> applications;
+
+    public Server() {
+        super();
+        this.applications = new HashSet<>();
+    }
+
+    public Server(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Datacenter getDatacenter() {
+        return datacenter;
+    }
+
+    public void setDatacenter(Datacenter datacenter) {
+        this.datacenter = datacenter;
+    }
+
+    public void addApplication(Application application) {
+        this.applications.add(application);
+    }
+
+    public Set<Application> getApplications() {
+        return this.applications;
+    }
 }
