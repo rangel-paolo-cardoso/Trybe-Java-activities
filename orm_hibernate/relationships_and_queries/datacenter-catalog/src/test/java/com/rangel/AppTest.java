234f0eeeec05cdff454adb302d21bf574536d8de
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-12 23:18:26 -0300

    Datacenter class implemented

--- orm_hibernate/relationships_and_queries/datacenter-catalog/src/test/java/com/rangel/AppTest.java
+++ orm_hibernate/relationships_and_queries/datacenter-catalog/src/test/java/com/rangel/AppTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.rangel.entities.Application;
+import com.rangel.entities.Datacenter;
 import com.rangel.entities.Server;
 import com.rangel.services.IService;
 
