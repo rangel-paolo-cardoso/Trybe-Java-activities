8390b22526a3059054d6b02a07a67b20d51799c4
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-29 22:30:12 -0300

    Addd and findAll working fine

--- java_web/how_does_java_go_to_the_web/api-rest-spring/src/main/java/com/rangel/spring/apirestspring/application/BookController.java
+++ java_web/how_does_java_go_to_the_web/api-rest-spring/src/main/java/com/rangel/spring/apirestspring/application/BookController.java
@@ -1,5 +1,38 @@
 package com.rangel.spring.apirestspring.application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.rangel.spring.apirestspring.domain.Book;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+@Controller
+@Path("/api/books")
 public class BookController {
     
+    private List<Book> books = new ArrayList<>();
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response add(Book book) {
+        books.add(book);
+        return Response.ok(book).build();
+    }
+
+    @GET
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response findAll() {
+        return Response.ok(books).build();
+    }
 }
