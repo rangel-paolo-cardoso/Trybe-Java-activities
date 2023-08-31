fafd8c8b838f3516f7bf1661da3a43ea2e9de913
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-30 23:51:59 -0300

    Added PUT and DELETE endpoints

--- java_web/how_does_java_go_to_the_web/api-rest-spring/src/main/java/com/rangel/spring/apirestspring/application/BookController.java
+++ java_web/how_does_java_go_to_the_web/api-rest-spring/src/main/java/com/rangel/spring/apirestspring/application/BookController.java
@@ -2,6 +2,8 @@ package com.rangel.spring.apirestspring.application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.rangel.spring.apirestspring.domain.Book;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
 
@@ -34,5 +39,37 @@ public class BookController {
     @Produces("application/json")
     public Response findAll() {
         return Response.ok(books).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response updateById(@PathParam("id") UUID id, Book bookEntity) {
+        try {
+        Book book = books.stream().filter(b -> b.getId().equals(id)).findAny().orElseThrow();
+        book.setName(bookEntity.getName());
+        book.setAuthor(bookEntity.getAuthor());
+        return Response.ok(book).build();
+        } catch (NoSuchElementException e) {
+        return Response.status(404).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response deleteById(@PathParam("id") UUID id) {
+        try {
+        Book book = books.stream()
+            .filter(b -> b.getId().equals(id))
+            .findAny()
+            .orElseThrow();
+        books.remove(book);
+        return Response.ok(book).build();
+        } catch (NoSuchElementException e) {
+        return Response.status(404).build();
+        }
     }
 }
